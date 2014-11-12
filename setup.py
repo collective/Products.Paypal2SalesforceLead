b7df5738f3c7b1ca3af39240708fab2431924959
Bryan Wilson <bryanlandia@gmail.com>
2014-11-11 19:24:37 -0800

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "Products.Paypal2SalesforceLead",
-    version = "1.2",
+    version = "1.2.1",
     description = "Paypal provides a service called Instant Payment Notification (IPN) which can notify a URL when a payment is made.  Paypal2SalesforceLead is a very simple Product for Plone which sets up a listener for IPN, and adds a new Lead to a Salesforce.com account using a Salesforce web-to-lead form whenever a new payment is made. See https://www.paypal.com/us/cgi-bin/webscr?cmd=p/xcl/rec/ipn-intro-outside for more background on Paypal IPN.",
     author = "David Glick",
     author_email = "david@glicksoftware.com",
